192a19f04a46c7d578f4b0e39564869b8baa4d01
Harsh Gupta <harshhvg999@gmail.com>
2021-06-01 14:42:07 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'myApp',
+      title: 'NLIDB',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xFF103597),
